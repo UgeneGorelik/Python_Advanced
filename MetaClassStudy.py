c37ba2299b62efd1e35cbe740afd9a1ecfd18dab
Ivgeny Gorelik <34338008+UgeneGorelik@users.noreply.github.com>
2018-02-08 17:03:06 +0200

    Update MetaClassStudy.py

--- MetaClassStudy.py
+++ MetaClassStudy.py
@@ -1,14 +1,31 @@
 import dis
 
+
+#
+#all classes without definition are taken from !!type!!
+#now we can declare that the class will be not created using type
+#but using some other thing other than DIRECTLY type
+#here we declarre a class that is instance of class type
+#
 class BaseMeta(type):
+    #here we define what will happen when we instantiate
+    # a class from the class we created (BaseMeta)
     def __new__(cls, name, bases, body):
-       #print the contents of the class
+       #print the contents of the class we can put here any function
+       #that will be ran before the instantiation of the below subclass
         print('BaseMeta.__new__', cls, name, bases, body)
        #catch if not exists function
         # if not 'bar' in body:
         #     print("bad user class")
+
+        #here we actualy return the newly created class
         return super().__new__(cls, name, bases, body)
 
+#Duplicated from above
+#all classes without definition are taken from !!type!!
+#now we can declare that the class will be not created using type
+#but using some other thing other than DIRECTLY type
+#here we define it to be created by BaseMeta
 class Base(metaclass=BaseMeta):
     def foo(self):
         return self.bar()
